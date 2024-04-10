b6e54b277530fc8dae58d229a2a7c0df4cd84d11
Wiktor Jakubowski <qbagency@gmail.com>
2024-04-10 21:37:34 +0200

    Stronger validation for upload image

--- src/components/meals/create-meal-form.tsx
+++ src/components/meals/create-meal-form.tsx
@@ -21,6 +21,14 @@ import { Textarea } from "../ui/textarea";
 import { Input } from "@/components/ui/input";
 import { toast } from "@/components/ui/use-toast";
 
+const MAX_FILE_SIZE = 3000000;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
 const FormSchema = z.object({
   name: z.string().min(2, {
     message: "User name must be at least 2 characters.",
@@ -39,8 +47,12 @@ const FormSchema = z.object({
   }),
   image: z
     .any()
-    .refine((file) => file?.length == 1, "File is required.")
-    .refine((file) => file[0]?.size <= 3000000, `Max file size is 3MB.`),
+    .refine((files) => files?.length == 1, "Image is required.")
+    .refine((files) => files[0]?.size <= MAX_FILE_SIZE, `Max file size is 3MB.`)
+    .refine(
+      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+      ".jpg, .jpeg, .png and .webp files are accepted.",
+    ),
 });
 
 export default function CreateMealForm() {
